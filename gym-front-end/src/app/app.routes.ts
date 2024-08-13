48908a5a665dd042c4233b5900dd8df4e5a5fdf6
Franco_Serjen <francoserjen100@gmail.com>
2024-08-13 17:48:35 -0300

    Implementación de ruteo con routerLink para ir al formulario de registro desde el formulario de loggin

--- gym-front-end/src/app/app.routes.ts
+++ gym-front-end/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import RegisterComponent from './components/auth/register/register.component';
 
 export const routes: Routes = [
-    { path: 'register', loadComponent: () => import('./components/auth/register/register.component')},
-    { path: '', loadComponent: () => import('./components/auth/loggin/loggin.component')}
+    { path: '', loadComponent: () => import('./components/auth/loggin/loggin.component') },
+    { path: 'register', loadComponent: () => import('./components/auth/register/register.component') }
 ];
